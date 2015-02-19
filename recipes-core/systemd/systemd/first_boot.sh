4bca654f48c6f5cc28b8df565467990f79feba85
Andrew Goodbody <andrew.goodbody@cambrionix.com>
2015-02-19 15:34:59 +0000

    Allow for incorrect use of unbooted flag

--- recipes-core/systemd/systemd/first_boot.sh
+++ recipes-core/systemd/systemd/first_boot.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+if [ -e /boot/unbooted ]; then
+  mv /boot/unbooted /boot/never_booted
+  reboot
+fi
+
 if [ -e /boot/never_booted -a -e /boot/first_boot ]; then
-  PSE=$(ps -e|grep esusbsrvr)
-  if [ "${PSE}" != "" ]; then
-    rm /boot/never_booted
-    rm /boot/first_boot
-  fi
+  rm /boot/never_booted
+  rm /boot/first_boot
 fi
